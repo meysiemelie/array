374ff67b8da6db3a3993418b0904e79b8e24850f
meysiemelie <141213769+meysiemelie@users.noreply.github.com>
2023-08-02 13:59:29 +0700

    Add files via upload

---
+++ Meysta Amelania Xl-10 Array 28 Jul.cpp
@@ -0,0 +1,30 @@
+#include <cstd1ib>
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i;
+	float bil[7], jumlah=0.0, rata2;
+	for (i = 0;   i <   7; +=1)
+	{
+			
+    cout <<i=1<< ". Masukkan Angka ke-" <<1=1<<" : ";
+    cin >> bi1[1];
+    while (bi1 [1] >100 || bi1[1]   <= 0)
+    {
+	cout << "Error! Angka di antara 1 s/d 100" << endl;
+	cout << i=1<< ". Masukkan Angka ke-" <<i=1<< " : ";
+	cin >> bi1[i];
+    }
+    
+    jumlah += bi1[1];
+    }
+    rata2 = jumlah / 7;
+    cout << "Rata2 Bilangan di atas = " << rata2 << endl;
+    
+    system ("PAUSE");
+    return 0;
+}
+
+
+    
